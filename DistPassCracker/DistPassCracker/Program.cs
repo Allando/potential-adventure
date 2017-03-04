5e280fd44692878d5e9a5734936ff086cb21b011
TRiBByX <christoffer.ssj@gmail.com>
2017-03-04 04:47:33 +0100

    Added threading example and test. Just for convenience

--- DistPassCracker/DistPassCracker/Program.cs
+++ DistPassCracker/DistPassCracker/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DistPassCracker
@@ -10,7 +12,24 @@ namespace DistPassCracker
     {
         static void Main(string[] args)
         {
-             //reddit was here
+            //Threading example
+            ThreadStart crackValThread1 = new ThreadStart(CrackerThread);
+            Console.WriteLine("Thread Created");
+            Thread crack1Child = new Thread(crackValThread1);
+            crack1Child.Name = "thread1";
+            crack1Child.Start();
+            Console.WriteLine($"Thread {crack1Child.Name} has been started");
+            Console.ReadKey();
+        }
+
+        static void CrackerThread()
+        {
+            //TEST af thread component.
+            //TODO: Functionality, basically doing some dictionary handling and so on.
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Womble is a faggot");
+            }
         }
     }
 }

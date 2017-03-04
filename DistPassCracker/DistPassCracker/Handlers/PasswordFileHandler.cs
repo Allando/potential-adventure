11137350879ddf1e544386d6014d2a5489ecd9bd
TRiBByX <christoffer.ssj@gmail.com>
2017-03-04 23:05:13 +0100

    Added PasswordFileHandler and StringHandler to project.

---
+++ DistPassCracker/DistPassCracker/Handlers/PasswordFileHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace DistPassCracker.Handlers
+{
+    public class PasswordFileHandler
+    {
+        public static List<Userinfo> ReadPasswordFile(string filename)
+        {
+            List<Userinfo> result = new List<Userinfo>();
+
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] parts = line.Split(":".ToCharArray());
+                    Userinfo usrinf = new Userinfo(parts[0], parts[1]);
+                    result.Add(usrinf);
+                }
+                return result;
+            }
+        }
+    }
+}
